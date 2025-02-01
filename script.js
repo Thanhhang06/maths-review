4ecbfd80dafd14a9c532c4f9110eedbbae383c0a
Thanhhang06 <hbrandy94@gmail.com>
2025-02-01 20:28:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // Cấu hình Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCUWgc1VPqgpNnIZKpYqFssdjZAB_QYUQk",
+  authDomain: "maths-login.firebaseapp.com",
+  projectId: "maths-login",
+  storageBucket: "maths-login.firebasestorage.app",
+  messagingSenderId: "547386894786",
+  appId: "1:547386894786:web:74af1e2f9ff689fcbc4e5b",
+  measurementId: "G-CJP6KPRRML"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
