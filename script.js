efc5cfdddd2f844f4e1d27301af6e3c1c094c35f
Thanhhang06 <hbrandy94@gmail.com>
2025-02-01 20:08:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -81,12 +81,13 @@ function login() {
 }
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCUWgc1VPqgpNnIZKpYqFssdjZAB_QYUQk",
+  authDomain: "maths-login.firebaseapp.com",
+  projectId: "maths-login",
+  storageBucket: "maths-login.firebasestorage.app",
+  messagingSenderId: "547386894786",
+  appId: "1:547386894786:web:74af1e2f9ff689fcbc4e5b",
+  measurementId: "G-CJP6KPRRML"
 };
 
 function loadUsers() {
